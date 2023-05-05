63cfbf6e871d601f0e2b6dc1d784a82e281756db
--Explosion-- <explosion.scratch@gmail.com>
2023-05-05 18:57:47 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<!-- Delete this stuff! -->
+<script>
+  let count = 0;
+</script>
+
+<h1>Hello world</h1>
+<span>
+  This is a template which supports less, GitHub pages deploy and more!
+</span>
+<button on:click={() => count++}>You've clicked {count} times</button>
+
+<style lang="less">
+  @import "main.less";
+  :global(body) {
+    .flex();
+    h1 {
+      font-weight: 100;
+      font-style: italic;
+      color: fade(@text, 90%);
+    }
+    span {
+      .font.light();
+      font-size: 1.2rem;
+    }
+    button {
+      margin-top: 1rem;
+      .button();
+    }
+  }
+</style>
